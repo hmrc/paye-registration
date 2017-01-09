45e9805b3ac64c279ca1a9aac27c236a5ae038dd
Thomas Stacey <thomas.stacey@digital.hmrc.gov.uk>
2017-01-09 11:14:28 +0000

    Add test endpoints

---
+++ test/controllers/test/TestEndpointControllerSpec.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.test
+
+import fixtures.{MongoFixture, AuthFixture}
+import helpers.PAYERegSpec
+import repositories.RegistrationMongoRepository
+import play.api.test.FakeRequest
+import play.api.http.Status
+import org.mockito.Matchers
+import org.mockito.Mockito._
+
+import scala.concurrent.Future
+
+class TestEndpointControllerSpec extends PAYERegSpec with AuthFixture with MongoFixture {
+
+  val mockRepo = mock[RegistrationMongoRepository]
+
+  class Setup {
+    val controller = new TestEndpointController {
+      override val auth = mockAuthConnector
+      override val registrationRepository = mockRepo
+    }
+  }
+
+  "Teardown registration collection" should {
+    "return a 200 response for success" in new Setup {
+      when(mockRepo.dropCollection).thenReturn(Future.successful(()))
+
+      val response = await(controller.registrationTeardown()(FakeRequest()))
+      status(response) shouldBe Status.OK
+    }
+
+    "return a 500 response for failure" in new Setup {
+      when(mockRepo.dropCollection).thenReturn(Future.failed(new RuntimeException("test failure message")))
+
+      val response = await(controller.registrationTeardown()(FakeRequest()))
+      status(response) shouldBe Status.INTERNAL_SERVER_ERROR
+    }
+  }
+
+  "Delete Registration" should {
+    "return a 200 response for success" in new Setup {
+      when(mockRepo.deleteRegistration(Matchers.any())).thenReturn(Future.successful(successfulWriteResult))
+
+      val response = await(controller.deleteRegistration("AC123456")(FakeRequest()))
+      status(response) shouldBe Status.OK
+    }
+
+    "return a 500 response for failure" in new Setup {
+      when(mockRepo.deleteRegistration(Matchers.any())).thenReturn(Future.failed(new RuntimeException("test failure message")))
+
+      val response = await(controller.deleteRegistration("AC123456")(FakeRequest()))
+      status(response) shouldBe Status.INTERNAL_SERVER_ERROR
+    }
+  }
+
+}
