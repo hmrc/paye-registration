8ebe184e6e05692fca9ca968bab6f9f19afddda5
Henri Lay <henri.lay@digital.hmrc.gov.uk>
2017-04-21 15:19:08 +0100

    add DES Top up audit event

---
+++ test/audit/DesTopUpAuditEventDetailSpec.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package audit
+
+import models.submission.{DESSubmission, TopUpDESSubmission}
+import play.api.libs.json.{JsObject, Json}
+import uk.gov.hmrc.play.test.UnitSpec
+
+class DesTopUpAuditEventDetailSpec extends UnitSpec {
+  "DesTopUpAuditEventDetail" should {
+    val regId = "123456789"
+
+    "construct full json as per definition" in {
+      val validTopUpDESSubmission = TopUpDESSubmission("ackRef", "accepted", Some("AA123456"))
+
+      val expected = Json.parse(
+        s"""
+           |{
+           |   "journeyId": "$regId",
+           |   "acknowledgementReference": "ackRef",
+           |   "incorporationStatus": "accepted",
+           |   "crn": "AA123456"
+           |}
+        """.stripMargin)
+
+      val testModel = DesTopUpAuditEventDetail(
+        regId,
+        Json.toJson[DESSubmission](validTopUpDESSubmission).as[JsObject]
+      )
+      Json.toJson(testModel)(DesTopUpAuditEventDetail.writes) shouldBe expected
+    }
+  }
+}
