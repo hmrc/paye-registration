52eb6d052cdcbbf5e68ac7f9c1ec8f2442463819
christopher.walker <christopher.walker@digital.hmrc.gov.uk>
2017-06-26 10:55:48 +0100

    removed depracted OneAppPerSuite

--- it/itutil/IntegrationSpecBase.scala
+++ it/itutil/IntegrationSpecBase.scala
@@ -17,11 +17,11 @@ package itutil
 
 import org.scalatest._
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
-import org.scalatestplus.play.OneServerPerSuite
+import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import uk.gov.hmrc.play.test.UnitSpec
 
 trait IntegrationSpecBase extends UnitSpec
-  with OneServerPerSuite with ScalaFutures with IntegrationPatience with Matchers
+  with GuiceOneServerPerSuite with ScalaFutures with IntegrationPatience with Matchers
   with WiremockHelper with BeforeAndAfterEach with BeforeAndAfterAll {
 
   override def beforeEach() = {
