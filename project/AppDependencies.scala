107cadf705ce063e10858eca6635064d4da56d16
eric.ohwotu <eric.ohwotu@digital.hmrc.gov.uk>
2018-07-24 09:47:03 +0100

    Dependency uplift

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,9 +6,9 @@ private object AppDependencies {
 }
 
 object MainDependencies {
-  private val microserviceBootstrapVersion  = "6.17.0"
+  private val microserviceBootstrapVersion  = "6.18.0"
   private val playUrlBindersVersion         = "2.1.0"
-  private val domainVersion                 = "5.1.0"
+  private val domainVersion                 = "5.2.0"
   private val playReactivemongoVersion      = "5.2.0"
   private val mongoLockVersion              = "4.1.0"
   private val playSchedulingVersion         = "4.1.0"
